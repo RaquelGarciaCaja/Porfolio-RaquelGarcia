8696beb590f479b7a8f59bf3a497ab7edafd9fa4
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-10-22 21:17:31 +0200

    add api and paint movies

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
 "use strict";
+const input = document.querySelector(".js-input");
+const btn = document.querySelector(".js-button");
+const listMovies = document.querySelector(".main__list");
+const url = "http://api.tvmaze.com/search/shows?q=girls";
+
+//api
+let movies = "";
+function getData() {
+  fetch(url + input.value)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      movies = data;
+      paintMovies();
+    });
+}
+getData();
+
+//paint
+let printMovie;
+function paintMovies() {
+  for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i].show);
+    listMovies.innerHTML += `<li class = "main__list-movie">`;
+    listMovies.innerHTML += `<img class = "main__list--img src = "${movies[i].show.image.medium}"></img>`;
+    listMovies.innerHTML += `<p class = "main__list--name ">Nombre: ${movies[i].show.name}</p>`;
+    listMovies.innerHTML += `<p class = "main__list--genres " >Genero: ${movies[i].show.genres}</p>`;
+    listMovies.innerHTML += "</li>";
+  }
+}
+
+btn.addEventListener("click", paintMovies);
